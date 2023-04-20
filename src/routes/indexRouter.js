159c0450a88551b3834d5e354545b37bc0c08ea8
Svetlana <sonikolaeva18@yandex.ru>
2023-04-20 16:30:57 +0300

    gg

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,20 +1,25 @@
 import express from 'express';
 import notAuth from '../middlewares/notAuth';
-import { Recipe } from '../../db/models';
 import isAuth from '../middlewares/isAuth';
+import { Recipe } from '../../db/models';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', async (req, res) => {
-  const allRecipe = await Recipe.findAll()
-  const initState = {allRecipe}
-  res.render('Layout', initState);
-});
+    const allRecipe = await Recipe.findAll()
+    const initState = {allRecipe}
+    res.render('Layout', initState);
+  });
 indexRouter.get('/signup', notAuth, (req, res) => {
   res.render('Layout');
 });
 
 indexRouter.get('/login', notAuth, (req, res) => {
   res.render('Layout');
+});
+indexRouter.get('/:id', isAuth, async (req, res) => {
+  const oneCook = await Recipe.findByPk(req.params.id);
+  const initstate = { oneCook };
+  res.render('Layout', initstate);
 });
 export default indexRouter;
